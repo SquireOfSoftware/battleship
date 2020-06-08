8c560bdbaa02bc1ca3b8e070d96738f3a59f6e00
Joseph Tran <fred.smoth@gmail.com>
2020-06-08 21:22:00 +1000

    Updates

---
+++ precache-manifest.37e04eb93bfb64dc184e241586cb8f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef57052dd0d488a09de5c42402235ed2",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "f09cad3042927538958f",
+    "url": "/battleship/static/css/main.ede2d405.chunk.css"
+  },
+  {
+    "revision": "ab3b31b115560831f2e5",
+    "url": "/battleship/static/js/2.7ca5dab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.7ca5dab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09cad3042927538958f",
+    "url": "/battleship/static/js/main.89402053.chunk.js"
+  },
+  {
+    "revision": "fa2bb6d32141478767f3",
+    "url": "/battleship/static/js/runtime-main.ceb60dc6.js"
+  }
+]);
